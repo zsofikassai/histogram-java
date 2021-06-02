5a2d892b5044da6c999e4228adee13f08a159bda
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-06-02 14:30:22 +0200

    Write tests for Range constructor

--- src/test/java/com/codecool/histogram/RangeTest.java
+++ src/test/java/com/codecool/histogram/RangeTest.java
@@ -1,9 +1,29 @@
 package com.codecool.histogram;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RangeTest {
-    
+    /*
+Range class is covered with tests, by implementing the RangeTest class.
+Provide test cases for constructor. The following cases should be covered:
+from < 0
+to < from
+Provide test cases for isInRange(). The following cases should be covered: - word's length in range - word's length equals to range from - word's length equals to range to - word's length is out of range
+Provide test case for a toString() method */
+
+    @Test
+    void range_fromLessThan0_throwException(){
+        assertThrows(IllegalArgumentException.class,() -> new Range(-1, 2));
+    }
+
+    @Test
+    void range_toLessThanFrom_throwException(){
+        assertThrows(IllegalArgumentException.class,() -> new Range(6, 2));
+    }
+
+
+
 }
