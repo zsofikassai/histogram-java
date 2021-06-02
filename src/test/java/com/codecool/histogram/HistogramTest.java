0857817be625968ae0bc9695da8709c08bd35ef9
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-06-02 12:11:21 +0200

    Write tests for generateRanges method

--- src/test/java/com/codecool/histogram/HistogramTest.java
+++ src/test/java/com/codecool/histogram/HistogramTest.java
@@ -6,7 +6,35 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HistogramTest {
+    Histogram histogramUnderTest;
 
+    @BeforeEach
+    void setUp() {
+        histogramUnderTest = new Histogram();
+    }
+
+    @Test
+    @Order(1)
+    void generateRanges_stepLessThan0_throwsException(){
+        assertThrows(IllegalArgumentException.class, () -> histogramUnderTest.generateRanges(-1, 1));
+    }
+    @Test
+    @Order(2)
+    void generateRanges_amountLessThan0_throwsException(){
+        assertThrows(IllegalArgumentException.class, () -> histogramUnderTest.generateRanges(1, -1));
+    }
+    @Test
+    @Order(3)
+    void generateRanges_positiveIntParameters_generateRanges(){
+        Range expectedRange1 = new Range(1, 5);
+        Range expectedRange2 = new Range(6, 10);
+        List<Range> expectedRanges = new ArrayList<>();
+        expectedRanges.add(expectedRange1);
+        expectedRanges.add(expectedRange2);
+        assertEquals(expectedRanges,
+                histogramUnderTest.generateRanges(5, 2));
+    }
 }
