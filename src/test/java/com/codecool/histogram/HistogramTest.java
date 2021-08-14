fe41a7313aea9e0b67d7e75c17496d9682d829ff
zsofikassai <zsofiaszonja.kassai@gmail.com>
2021-08-14 15:30:36 +0200

    Add unit test for generating with words in range

--- src/test/java/com/codecool/histogram/HistogramTest.java
+++ src/test/java/com/codecool/histogram/HistogramTest.java
@@ -10,6 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HistogramTest {
     Histogram histogramUnderTest;
+    Range expectedRange1 = new Range(1, 5);
+    Range expectedRange2 = new Range(6, 10);
+    List<Range> expectedRanges = new ArrayList<>();
+
 
     @BeforeEach
     void setUp() {
@@ -26,15 +30,27 @@ public class HistogramTest {
     void generateRanges_amountLessThan0_throwsException(){
         assertThrows(IllegalArgumentException.class, () -> histogramUnderTest.generateRanges(1, -1));
     }
+
     @Test
     @Order(3)
     void generateRanges_positiveIntParameters_generateRanges(){
-        Range expectedRange1 = new Range(1, 5);
-        Range expectedRange2 = new Range(6, 10);
-        List<Range> expectedRanges = new ArrayList<>();
         expectedRanges.add(expectedRange1);
         expectedRanges.add(expectedRange2);
         assertEquals(expectedRanges,
                 histogramUnderTest.generateRanges(5, 2));
     }
+
+    @Test
+    @Order(4)
+    void generate_wordsInRanges(){
+        expectedRanges.add(expectedRange1);
+        expectedRanges.add(expectedRange2);
+        Map<Range, Integer> expected = new HashMap<>();
+        expected.put(expectedRange1, 5);
+        assertEquals(expected, histogramUnderTest.generate("Hello lkk k 44 hello hello", expectedRanges));
+    }
+
+    
+
+
 }
